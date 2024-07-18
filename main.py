aeb0984d056617aa8a245d750287fe43cfe449fb
staslusenko <lusenkostas177@gmail.com>
2024-07-18 13:21:27 +0200

    додали діні про погодц

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 from kivy.lang import Builder
+
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
-from kivymd.uix.label import MDLabel
+from kivymd.uix.label import MDLabel 
+import requests
 from kivymd.uix.button import MDRectangleFlatIconButton
+from kivymd.uix.card import MDCard
+from setings import API_KEY, WEATHER_URL
 
+class WeatherCard(MDCard):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 class HomeScreen(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+    def get_weather_data(self, url, city):
+        """Функція робить запит до сайту погоди і повертає JSON файл з результатом"""
+
+        api_params = {
+            "q": city,
+            "appid": API_KEY
+        }
+        data = requests.get(url, api_params)
+        response = data.json()
+        print(response)
+        return response
+    def search(self):
+        city = self.ids.city_name.text.lower().strip()
+        print(city)
+
+
+        
 
 
 class MainApp(MDApp):
---
+++ setings.py
@@ -0,0 +1,8 @@
+API_KEY = "4170c1d06aac3e2c19cfe31ff11640c4"
+WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&lang=uk"
+
+
+
+
+
+
